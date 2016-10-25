f5d78fdb4dae0a84ffec5f28e8a74dbfb9b72fb3
Michael S. Mikowski <mike.mikowski@gmail.com>
2016-10-24 22:53:17 -0700

    WIP istanbul + jsdom to provide code coverage

--- dist/pcss.cfg.js
+++ dist/pcss.cfg.js
@@ -10,6 +10,7 @@
   white : true,    todo : true,  unparam : true
 */
 /*global pcss */
+
 pcss._cfg_ = (function () {
   'use strict';
   var
@@ -152,7 +153,9 @@ pcss._cfg_ = (function () {
 
       // 1px increments for 16px nominal font-size
       // VIM tip: I used visual-select and tac reverse order
+      // to create negative values:
       // :'<'>!tac
+
       _n2rem_         : '-2rem',
       _n1rem_         : '-1rem',
       _nd9375rem_     : '-.9375rem', // 15px
@@ -195,17 +198,23 @@ pcss._cfg_ = (function () {
       _1d625rem_      : '1.625rem',
       _1d75rem_       : '1.75rem',
       _1d875rem_      : '1.875rem',
+
       // 4px increments for 16px nominal font-size
       _2rem_          : '2rem',
       _2d25rem_       : '2.25rem',
       _2d5rem_        : '2.5rem',
       _2d75rem_       : '2.75rem',
+
       // 8px increments for 16px nominal font-size
       _3rem_          : '3rem',
       _3d5rem_        : '3.5rem',
+      _3d75rem_       : '3.75rem',
       _4rem_          : '4rem',
       _4d5rem_        : '4.5rem',
       _5rem_          : '5rem',
+      _5d5rem_        : '5.5rem',
+      _6rem_          : '6rem',
+      _10rem_         : '10rem',
 
       _200_           : '200',
       _400_           : '400',
@@ -241,7 +250,7 @@ pcss._cfg_ = (function () {
       _ellipsis_      : 'ellipsis',
       _fixed_         : 'fixed',
       _font_fixed_    : 'courier new,courier,fixed,monospace',
-      _font_sans_     : 'arial,helvetica,sans-serif',
+      _font_sans_     : 'opensans,arial,helvetica,sans-serif',
       _hidden_        : 'hidden',
       _important_     : '!important',
       _inherit_       : 'inherit',
